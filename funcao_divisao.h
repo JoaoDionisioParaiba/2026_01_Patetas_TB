32ae735ec725af195fc0f0616d911cd70fb21e50
Giuseppe <145723894+giuseppinhu@users.noreply.github.com>
2026-06-15 20:08:45 -0300

    new function dividir

---
+++ funcao_divisao.h
@@ -0,0 +1,4 @@
+#ifndef FUNCAO_DIVISAO_H
+#define FUNCAO_DIVISAO_H
+    float dividir(float num1, float num2);
+#endif 
--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Inclua os headers das funções aqui
-// #include "funcao_soma.h"
-// #include "funcao_subtracao.h"
-// #include "funcao_multiplicacao.h"
-// #include "funcao_divisao.h"
+#include "funcao_divisao.h"
 
 int main() {
     printf("Bem-vindo ao nosso projeto de calculadora!\n");
@@ -22,11 +20,11 @@ int main() {
     // int resultado_multiplicacao = multiplicar(7, 6);
     // printf("Multiplicacao (7 * 6): %d\n", resultado_multiplicacao);
 
-    // float resultado_divisao = dividir(100.0, 4.0);
-    // printf("Divisao (100.0 / 4.0): %.2f\n", resultado_divisao);
+    float resultado_divisao = dividir(15.0, 3.0);
+    printf("Divisao (15.0 / 3.0): %.2f\n\n", resultado_divisao);
 
-    // float resultado_divisao_erro = dividir(10.0, 0.0); // Teste de erro
-    // printf("Divisao (10.0 / 0.0): %.2f\n", resultado_divisao_erro); // Deve indicar erro
+    float resultado_divisao_erro = dividir(10.0, 0.0); // Teste de erro
+    printf("Divisao (10.0 / 0.0): %.2f\n", resultado_divisao_erro); // Deve indicar erro
 
     return 0;
 }
